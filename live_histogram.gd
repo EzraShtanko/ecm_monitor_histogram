adaff794dc0cf38dcbd37916ce162dc4190a8a2d
Ezra (Den) Shtanko <ezra.shtanko@gmail.com>
2026-07-01 01:13:37 -0400

    added dynamically populated labels and test fill/clear functions

--- live_histogram.gd
+++ live_histogram.gd
@@ -20,7 +20,12 @@ const updated_LABELS	:= 0x01 << 2
 @export var bracket_min			: float = 0.:
 	set(v): bracket_min = v;	reconfig.post(updated_LABELS)
 @export_range(1, RESOLUTION - 1) var bracket_avg: int = 32
-@export var custom_label_settings: LabelSettings
+@export var custom_label_settings: LabelSettings:
+	set(v): custom_label_settings = v; reconfig.post(updated_LABELS)
+
+@export_tool_button("Test Fill") var action_fill_test: Callable = fill_test_buffer
+@export_tool_button("Clear Buffer") var action_clear_buffer: Callable = clear_buffer
+
 
 @export var color_nil: Color 	= Color(0.2, 		0.1, 	0., 	0.25):
 	set(v): color_nil = v;		reconfig.post(updated_COLORS)
@@ -45,10 +50,23 @@ class Reconfig extends mio.Reconfig:
 	var x: LiveHistogram
 	func _init(_x: LiveHistogram): x = _x
 	func _process(f: int = 0xffff) -> void:
-		(x.material as ShaderMaterial).set_shader_parameter(&"color_value_nil", 	x.color_nil)
-		(x.material as ShaderMaterial).set_shader_parameter(&"color_value_max", 	x.color_max)
-		(x.material as ShaderMaterial).set_shader_parameter(&"color_value_avg", 	x.color_avg)
-		(x.material as ShaderMaterial).set_shader_parameter(&"color_value_clip", 	x.color_clip)
+		if f & updated_COLORS:
+			(x.material as ShaderMaterial).set_shader_parameter(&"color_value_nil", 	x.color_nil)
+			(x.material as ShaderMaterial).set_shader_parameter(&"color_value_max", 	x.color_max)
+			(x.material as ShaderMaterial).set_shader_parameter(&"color_value_avg", 	x.color_avg)
+			(x.material as ShaderMaterial).set_shader_parameter(&"color_value_low",		x.color_low)
+			(x.material as ShaderMaterial).set_shader_parameter(&"color_value_clip", 	x.color_clip)
+		
+		if f & updated_LABELS:
+			x.label_name.text = x.label
+			x.label_max.text = "%5.2f" % x.bracket_max
+			x.label_min.text = "%5.2f" % x.bracket_min
+			
+			if x.custom_label_settings:
+				for i: Label in [x.label_name, x.label_min, x.label_max]:
+					i.label_settings = x.custom_label_settings
+			
+			pass
 var reconfig := Reconfig.new(self)
 
 
@@ -90,21 +108,41 @@ func on_ready() -> void:
 	if not $LabelMax:
 		label_max = Label.new()
 		label_max.name = "LabelMax"
-		label_max.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+		label_max.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
 		label_max.vertical_alignment = VERTICAL_ALIGNMENT_BOTTOM
 		add_child(label_max)
+		label_name.owner = self
 		label_max.text = "%5.2f" % bracket_max
+		label_max.force_update_transform()
+		
+		label_max.size.x = 50.
 		
 		label_max.anchor_top = 0.
 		label_max.anchor_bottom = 0.
 		label_max.anchor_left = 1.
 		label_max.anchor_right = 1.
+		
+		label_max.position = Vector2(size.x - 55., 0.)
 	elif not label_max:
 		label_max = $LabelMax
 	
 	
 	if not $LabelMin:
-		pass
+		label_min = Label.new()
+		label_min.name = "LabelMax"
+		label_min.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+		label_min.vertical_alignment = VERTICAL_ALIGNMENT_TOP
+		add_child(label_min)
+		label_min.text = "%5.2f" % bracket_min
+		
+		label_min.size.x = 50.
+		
+		label_min.anchor_top = 1.
+		label_min.anchor_bottom = 1.
+		label_min.anchor_left = 1.
+		label_min.anchor_right = 1.
+		
+		label_min.position = size - Vector2(55., label_min.size.y)
 	elif not label_min:
 		label_min = $LabelMin
 	
@@ -139,3 +177,14 @@ func log_value(v: float) -> void:
 func _exit_tree() -> void:
 	
 	pass
+
+
+func fill_test_buffer() -> void:
+	for i in RESOLUTION:
+		var a: float = float(i) / float(bracket_avg) * 2. * PI
+		buffer[i] = Vector2(lerp(bracket_min, bracket_max + 0.01, (sin(a) + 1.) / 2.), lerp(bracket_min, bracket_max, 0.5))
+	(material as ShaderMaterial).set_shader_parameter("buffer", buffer)
+
+func clear_buffer() -> void:
+	buffer.fill(Vector2(0., 0.))
+	(material as ShaderMaterial).set_shader_parameter("buffer", buffer)
